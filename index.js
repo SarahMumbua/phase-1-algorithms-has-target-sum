e27063e30e010805b3ee07cd69c57a52da3e22e8
SarahMumbua <mumbuasarah2030@gmail.com>
2023-04-08 22:01:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+
+    }
+
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+    1. Loop through all possible pairs of integers in the nums array
+    2. Check if the sum of the pair is equal to the target
+    3. If it is, return true
+    4. if no pair is found, return false
 */
 
 /*
@@ -17,6 +31,10 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 28));
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 59));
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
